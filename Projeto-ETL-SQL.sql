295780c2f1ed5bdaba71354fdbe2d25b16de4bed
Rodrigo Carrara <rodrigocarrara@live.com>
2024-07-28 21:40:26 -0300

    atualização dos arquivos

--- Projeto-ETL-SQL.sql
+++ Projeto-ETL-SQL.sql
@@ -1,4 +1,8 @@
 USE spotify_charts;
+
+-- Obter a maior e a menor data dos charts 
 SELECT MAX(date) from data_spotify_charts;
 SELECT MAX(date) from data_spotify_charts;
+
+-- Obter o número de linhas da base 
 SELECT COUNT(*) FROM data_spotify_charts;
